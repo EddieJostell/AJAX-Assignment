c4f08da05e1e4ae20a9aa7a83c60fa5a7c5f4bd4
Eddie <eddie.jostell@gmail.com>
2017-03-18 22:19:54 +0100

    api categories

--- js/script.js
+++ js/script.js
@@ -32,10 +32,10 @@ const newsMod = (() => {
       });
     },
 
-    getRedditNews: () => {
-     const url = " https://newsapi.org/v1/articles?source=reddit-r-all&sortBy=top&apiKey=ba003866cd1849ffb405924244eb308e";
+    getTopBBCNews: () => {
+     const urlBBC = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=ba003866cd1849ffb405924244eb308e";
 
-     fetch(url)
+     fetch(urlBBC)
 			.then((resp) => resp.json()) // Transform the data into json
 			.then(function(data) {
 				console.log(data);
@@ -44,8 +44,8 @@ const newsMod = (() => {
                   newsOutput.innerHTML = "";
                   for (var i = 0; i < news.length; i++) {
                     let newsDiv = `
-                    <div class="showNews col-md-12 col-sm-12 col-xs-12">
-                    <img height="400" width="695" src="${news[i].urlToImage}">
+                    <div class="showNews col-md-4">
+                    <img class="img-responsive pt-15" src="${news[i].urlToImage}">
                     <h5>Headline: ${news[i].title}</h5>
                     <h5>Author: ${news[i].author}</h5>
                     <p>Description: ${news[i].description}</p>
@@ -59,6 +59,60 @@ const newsMod = (() => {
         console.log(error);
       });		
     },
+    getTopCNNNews: () => {
+     const urlCnn = "https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=ba003866cd1849ffb405924244eb308e";
+
+     fetch(urlCnn)
+      .then((resp) => resp.json()) // Transform the data into json
+      .then(function(data) {
+        console.log(data);
+                  // Create and append the li's to the ul
+                  let news = data.articles;
+                  newsOutput.innerHTML = "";
+                  for (var i = 0; i < news.length; i++) {
+                    let newsDiv = `
+                    <div class="showNews col-md-4">
+                    <img class="img-responsive pt-15" src="${news[i].urlToImage}">
+                    <h5>Headline: ${news[i].title}</h5>
+                    <h5>Author: ${news[i].author}</h5>
+                    <p>Description: ${news[i].description}</p>
+                    <p>Date: ${news[i].publishedAt}</p>
+                    <a href="${news[i].url}">${news[i].url}</a>
+                    </div>`;
+                    newsOutput.innerHTML += newsDiv;
+                  };          
+                })
+      .catch(function(error) {
+        console.log(error);
+      });   
+    },
+    getGeneralNews: () => {
+      const urlCnn = "https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=ba003866cd1849ffb405924244eb308e";
+
+     fetch(urlCnn)
+      .then((resp) => resp.json()) // Transform the data into json
+      .then(function(data) {
+        console.log(data);
+                  // Create and append the li's to the ul
+                  let news = data.articles;
+                  newsOutput.innerHTML = "";
+                  for (var i = 0; i < news.length; i++) {
+                    let newsDiv = `
+                    <div class="showNews col-md-4">
+                    <img class="img-responsive pt-15" src="${news[i].urlToImage}">
+                    <h5>Headline: ${news[i].title}</h5>
+                    <h5>Author: ${news[i].author}</h5>
+                    <p>Description: ${news[i].description}</p>
+                    <p>Date: ${news[i].publishedAt}</p>
+                    <a href="${news[i].url}">${news[i].url}</a>
+                    </div>`;
+                    newsOutput.innerHTML += newsDiv;
+                  };          
+                })
+      .catch(function(error) {
+        console.log(error);
+      });   
+    }
 
     getSportsNews: () => {
       const url = "https://newsapi.org/v1/sources?category=sport&apiKey=ba003866cd1849ffb405924244eb308e";
@@ -112,9 +166,91 @@ const newsMod = (() => {
         };
       })
     },
+    getTechNews: () => {
+      const techUrl = "https://newsapi.org/v1/sources?category=technology&apiKey=ba003866cd1849ffb405924244eb308e";
+      
+      fetch(techUrl)
+      .then((resp) => resp.json()) // Transform the data into json
+      .then(function(data) {
+        console.log(data);
+        newsOutput.innerHTML = "";
+        let tech = data.sources;
 
+        for (var i = 0; i < tech.length; i++) {
+          let techDiv = `
+          <div class="showNews">
+          <img height="400" width="695" src="${tech[i].urlToLogos}">
+          <h5>Source: ${tech[i].name}</h5>
+          <h5>Category: ${tech[i].category}</h5>
+          <p>Description: ${tech[i].description}</p>
+          <p>Language: ${tech[i].language}</p>
+          <a href="${tech[i].url}">${tech[i].url}</a>
+          </div>`;
+          newsOutput.innerHTML += techDiv;
+        };
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    },
 
-  }
+    getMusicNews: () => {
+      const musicUrl = "https://newsapi.org/v1/sources?category=music&apiKey=ba003866cd1849ffb405924244eb308e";
+      
+      fetch(musicUrl)
+      .then((resp) => resp.json()) // Transform the data into json
+      .then(function(data) {
+        console.log(data);
+        newsOutput.innerHTML = "";
+        let music = data.sources;
+
+        for (var i = 0; i < music.length; i++) {
+          let musicDiv = `
+          <div class="showNews">
+          <img height="400" width="695" src="${music[i].urlToLogos}">
+          <h5>Source: ${music[i].name}</h5>
+          <h5>Category: ${music[i].category}</h5>
+          <p>Description: ${music[i].description}</p>
+          <p>Language: ${music[i].language}</p>
+          <p>Country: ${music[i].country}</p>
+          <a href="${music[i].url}">${music[i].url}</a>
+          </div>`;
+          newsOutput.innerHTML += musicDiv;
+        };
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    },
+    getGamingNews: () => {
+      const gameUrl = "https://newsapi.org/v1/sources?category=gaming&apiKey=ba003866cd1849ffb405924244eb308e";
+      
+      fetch(gameUrl)
+      .then((resp) => resp.json()) // Transform the data into json
+      .then(function(data) {
+        console.log(data);
+        newsOutput.innerHTML = "";
+        let game = data.sources;
+
+        for (var i = 0; i < game.length; i++) {
+          let gameDiv = `
+          <div class="showNews">
+          <img class="img-responsive pt-15" src="${game[i].urlToLogos}">
+          <h5>Source: ${game[i].name}</h5>
+          <h5>Category: ${game[i].category}</h5>
+          <p>Description: ${game[i].description}</p>
+          <p>Language: ${game[i].language}</p>
+          <p>Country: ${game[i].country}</p>
+          <a href="${game[i].url}">${game[i].url}</a>
+          </div>`;
+          newsOutput.innerHTML += gameDiv;
+        };
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
+}
 
 })();
 
@@ -123,3 +259,9 @@ document.getElementById("techNews").addEventListener("click", newsMod.getRedditN
 document.getElementById("latestNews").addEventListener("click", newsMod.getLatestNews);
 document.getElementById("sportsNews").addEventListener("click", newsMod.getSportsNews);
 document.getElementById("sourceOfNews").addEventListener("click", newsMod.sourceOfNews);
+document.getElementById("techNews").addEventListener("click", newsMod.getTechNews);
+document.getElementById("musicNews").addEventListener("click", newsMod.getMusicNews);
+document.getElementById("cnnNews").addEventListener("click", newsMod.getTopCNNNews);
+document.getElementById("bbcNews").addEventListener("click", newsMod.getTopBBCNews);
+document.getElementById("gameNews").addEventListener("click", newsMod.getGamingNews);
+
